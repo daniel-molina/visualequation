a04cf4c4aaac6bd0fb8758bbca87c5bba8bfe339
Daniel Molina García <lluvia@autistici.org>
2020-10-16 16:04:41 +0200

    Modifying Ops to support different properties per instance

---
+++ tests/test_ops.py
@@ -0,0 +1,393 @@
+#  visualequation is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  visualequation is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+
+from visualequation.ops import *
+
+
+class OpsTests(unittest.TestCase):
+    def test_pp_class(self):
+        pp = PublicProperties()
+        self.assertEqual(tuple(pp.keys()), PP_KEYS)
+        for v in pp.values():
+            self.assertEqual(v, None)
+
+        with self.assertRaises(TypeError):
+            pp = PublicProperties(8)
+        with self.assertRaises(TypeError):
+            pp = PublicProperties(None)
+        with self.assertRaises(KeyError):
+            pp = PublicProperties(wrongkw=False)
+        with self.assertRaises(KeyError):
+            pp = PublicProperties(wrongkw=None)
+
+        pp = PublicProperties(color=None)
+        self.assertEqual(tuple(pp.keys()), PP_KEYS)
+        self.assertEqual(pp["color"], None)
+
+        pp = PublicProperties(color=8)
+        self.assertEqual(tuple(pp.keys()), PP_KEYS)
+        self.assertEqual(pp["color"], 8)
+        self.assertEqual(pp["style"], None)
+        with self.assertRaises(KeyError):
+            pp["wrongkw2"]
+
+        pp = PublicProperties(color=8, style=5)
+        self.assertEqual(tuple(pp.keys()), PP_KEYS)
+        self.assertEqual(pp["color"], 8)
+        self.assertEqual(pp["style"], 5)
+        self.assertEqual(pp["font"], None)
+        with self.assertRaises(KeyError):
+            pp["wrongkw3"]
+
+        pp = PublicProperties(color=8, style=5, font=False)
+        self.assertEqual(tuple(pp.keys()), PP_KEYS)
+        self.assertEqual(pp["color"], 8)
+        self.assertEqual(pp["style"], 5)
+        self.assertEqual(pp["font"], False)
+        with self.assertRaises(KeyError):
+            pp["wrongkw4"]
+
+        with self.assertRaises(KeyError):
+            pp = PublicProperties(color=8, wrongkw5=5)
+
+        pp = PublicProperties()
+        self.assertEqual(pp["color"], None)
+        pp["color"] = 8
+        self.assertEqual(pp["color"], 8)
+        self.assertEqual(pp["style"], None)
+        pp["style"] = 9
+        self.assertEqual(pp["style"], 9)
+        with self.assertRaises(KeyError):
+            pp["wrongkw9"] = 3
+
+    def test_pseudo_symb_class(self):
+        psymb = PseudoSymb("a", "b")
+        self.assertEqual(psymb._name, "a")
+        self.assertEqual(psymb._latex_code, "b")
+        self.assertEqual(psymb.pp["color"], None)
+        psymb.pp["color"] = 3
+        self.assertEqual(psymb.pp["color"], 3)
+        with self.assertRaises(KeyError):
+            x = psymb.pp["wrongkw9"]
+        with self.assertRaises(KeyError):
+            psymb.pp["wrongkw9"] = 3
+
+        psymb1 = PseudoSymb("a", "b")
+        psymb2 = PseudoSymb("a", "b", color=None)
+        self.assertEqual(psymb1, psymb2)
+        self.assertIsNot(psymb1, psymb2)
+        psymb3 = PseudoSymb("a", "b", color=3)
+        self.assertNotEqual(psymb1, psymb3)
+
+        psymb4 = PseudoSymb("a", "b", PublicProperties())
+        self.assertEqual(psymb1, psymb4)
+        psymb5 = PseudoSymb("a", "b", PublicProperties(color=3))
+        self.assertEqual(psymb3, psymb5)
+        psymb4bis = PseudoSymb("a", "b", pp=PublicProperties())
+        self.assertEqual(psymb1, psymb4bis)
+        psymb5bis = PseudoSymb("a", "b", pp=PublicProperties(color=3))
+        self.assertEqual(psymb3, psymb5bis)
+
+        with self.assertRaises(KeyError):
+            psymb5 = PseudoSymb("a", "b", PublicProperties(dad=3))
+
+        with self.assertRaises(KeyError):
+            psymb5 = PseudoSymb("a", "b", pp=PublicProperties(dad=3))
+
+        with self.assertRaises(TypeError) as cm:
+            psymb5 = PseudoSymb("a", "b", 43)
+        self.assertEqual(cm.exception.args[0],
+                         PSEUDOSYMB_WRONG_PP_ARG_ERROR_MSG)
+
+        with self.assertRaises(TypeError) as cm:
+            psymb5 = PseudoSymb("a", "b", PublicProperties(color=3), style=4)
+        self.assertEqual(cm.exception.args[0], PSEUDOSYMB_PP_MIXED_ERROR_MSG)
+
+        with self.assertRaises(TypeError) as cm:
+            psymb5 = PseudoSymb("a", "b", pp=PublicProperties(color=3),
+                                style=4)
+        self.assertEqual(cm.exception.args[0], PSEUDOSYMB_PP_MIXED_ERROR_MSG)
+
+        with self.assertRaises(TypeError) as cm:
+            psymb5 = PseudoSymb("a", "b", PublicProperties(color=3), wrongkw=4)
+        self.assertEqual(cm.exception.args[0], PSEUDOSYMB_PP_MIXED_ERROR_MSG)
+
+    def test_pseudo_symb_repr(self):
+        psymb1 = PseudoSymb("a", "b")
+        self.assertEqual(
+            repr(psymb1),
+            "PseudoSymb('a', 'b')")
+        psymb2 = PseudoSymb("a", "b", font=2)
+        self.assertEqual(
+            repr(psymb2),
+            "PseudoSymb('a', 'b', font=2)")
+        psymb2.pp["style"] = 4
+        self.assertEqual(
+            repr(psymb2),
+            "PseudoSymb('a', 'b', font=2, style=4)")
+
+    def test_op_class(self):
+        op = Op("O", "B")
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 1)
+        self.assertEqual(op._pref_arg, 1)
+        self.assertEqual(op.pp["color"], None)
+
+        op = Op("O", "B", 3)
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 3)
+        self.assertEqual(op._pref_arg, 1)
+        self.assertEqual(op.pp["color"], None)
+
+        op = Op("O", "B", n_args=3)
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 3)
+        self.assertEqual(op._pref_arg, 1)
+        self.assertEqual(op.pp["color"], None)
+
+        op = Op("O", "B", 3, 2)
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 3)
+        self.assertEqual(op._pref_arg, 2)
+        self.assertEqual(op.pp["color"], None)
+
+        op = Op("O", "B", 3, user_sel=False)
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 3)
+        self.assertEqual(op._pref_arg, 1)
+        self.assertEqual(op._user_sel, False)
+        self.assertEqual(op.pp["color"], None)
+
+        op = Op("O", "B", 3, color=8)
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 3)
+        self.assertEqual(op._pref_arg, 1)
+        self.assertEqual(op._user_sel, True)
+        self.assertEqual(op.pp["color"], 8)
+
+        op = Op("O", "B", 3, pp=PublicProperties(color=8))
+        self.assertEqual(op._name, "O")
+        self.assertEqual(op._latex_code, "B")
+        self.assertEqual(op._n_args, 3)
+        self.assertEqual(op._pref_arg, 1)
+        self.assertEqual(op._user_sel, True)
+        self.assertEqual(op.pp["color"], 8)
+
+    def test_op_repr(self):
+        op = Op("a", "b")
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b')")
+        op = Op("a", "b", font=2)
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', font=2)")
+        op.pp["style"] = 4
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', font=2, style=4)")
+
+        op = Op("a", "b", n_args=2)
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', n_args=2)")
+
+        op = Op("a", "b", 3, pref_arg=2)
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', n_args=3, pref_arg=2)")
+
+        op = Op("a", "b", lo_base=True)
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', lo_base=True)")
+
+        op = Op("a", "b", user_sel=False)
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', user_sel=False)")
+
+        op = Op("a", "b", n_args=2, style=3)
+        self.assertEqual(
+            repr(op),
+            "Op('a', 'b', style=3, n_args=2)")
+
+    def test_op_rstep(self):
+        for n_args in range(1, 6):
+            op = Op("a", "b", n_args)
+
+            self.assertEqual(op.rstep(None), 1)
+            self.assertEqual(op.rstep(n_args), None)
+
+            for n in range(1, n_args):
+                self.assertEqual(op.rstep(n), n + 1)
+
+            for n in range(-6, 1):
+                with self.assertRaises(ValueError):
+                    print(op.rstep(n))
+
+            for n in range(n_args+1, n_args+6):
+                with self.assertRaises(ValueError):
+                    print(op.rstep(n))
+
+    def test_op_lstep(self):
+        for n_args in range(1, 6):
+            op = Op("a", "b", n_args)
+
+            self.assertEqual(op.lstep(1), None)
+            self.assertEqual(op.lstep(None), n_args)
+
+            for n in range(2, n_args+1):
+                self.assertEqual(op.lstep(n), n - 1)
+
+            for n in range(-6, 1):
+                with self.assertRaises(ValueError):
+                    print(op.lstep(n))
+
+            for n in range(n_args+1, n_args+6):
+                with self.assertRaises(ValueError):
+                    print(op.lstep(n))
+
+    def test_op_ustep_dstep(self):
+        for n_args in range(1, 6):
+            op = Op("a", "b", n_args)
+
+            self.assertIsNone(op.ustep(None))
+            self.assertIsNone(op.dstep(None))
+
+            for n in range(1, n_args + 1):
+                self.assertIsNone(op.ustep(n))
+                self.assertIsNone(op.dstep(n))
+
+            for n in range(-6, 1):
+                with self.assertRaises(ValueError):
+                    print(op.ustep(n))
+                with self.assertRaises(ValueError):
+                    print(op.dstep(n))
+
+            for n in range(n_args + 1, n_args + 6):
+                with self.assertRaises(ValueError):
+                    print(op.ustep(n))
+                with self.assertRaises(ValueError):
+                    print(op.dstep(n))
+
+    def test_pjuxt(self):
+        pj = PJuxt()
+        self.assertEqual(pj.current_n, 2)
+        self.assertEqual(pj._name, "pjuxt")
+        self.assertEqual(pj._latex_code, "")
+        self.assertEqual(pj._n_args, -1)
+        self.assertEqual(pj.pp, PublicProperties())
+
+        pj.current_n = 4
+        self.assertEqual(pj.current_n, 4)
+        pj2 = PJuxt(4)
+        self.assertEqual(pj, pj2)
+
+        pj = PJuxt(pp=PublicProperties(color=3, style=5))
+        self.assertEqual(pj.current_n, 2)
+        self.assertEqual(pj._name, "pjuxt")
+        self.assertEqual(pj._latex_code, "")
+        self.assertEqual(pj._n_args, -1)
+        self.assertEqual(pj.pp, PublicProperties(color=3, style=5))
+
+        with self.assertRaises(TypeError):
+            PJuxt(PublicProperties(color=3, style=5))
+
+        for n in range(-5, 2):
+            with self.assertRaises(ValueError):
+                PJuxt(n)
+
+    def test_pjuxt_rstep(self):
+        for n_args in range(2, 6):
+            pj = PJuxt(n_args)
+
+            self.assertEqual(pj.rstep(None), 1)
+            self.assertEqual(pj.rstep(n_args), None)
+
+            for n in range(1, n_args):
+                self.assertEqual(pj.rstep(n), n + 1)
+
+            for n in range(-6, 1):
+                with self.assertRaises(ValueError):
+                    print(pj.rstep(n))
+
+            for n in range(n_args + 1, n_args + 6):
+                with self.assertRaises(ValueError):
+                    print(pj.rstep(n))
+
+    def test_pjuxt_lstep(self):
+        for n_args in range(2, 6):
+            pj = PJuxt(n_args)
+
+            self.assertEqual(pj.lstep(1), None)
+            self.assertEqual(pj.lstep(None), n_args)
+
+            for n in range(2, n_args+1):
+                self.assertEqual(pj.lstep(n), n - 1)
+
+            for n in range(-6, 1):
+                with self.assertRaises(ValueError):
+                    print(pj.lstep(n))
+
+            for n in range(n_args+1, n_args+6):
+                with self.assertRaises(ValueError):
+                    print(pj.lstep(n))
+
+    def test_pjuxt_tjuxt(self):
+        for kw in (PublicProperties(color=4),
+                   PublicProperties(style=3, font=5),
+                   PublicProperties(style=3, font=3, color=5)):
+            pj = PJuxt(2, pp=kw)
+            tj = pj.equiv_tjuxt()
+            pj._name = "tjuxt"
+            pj._user_sel = True
+            self.assertEqual(pj.__dict__, tj.__dict__)
+
+    def test_tjuxt(self):
+        pj = TJuxt()
+        self.assertEqual(pj.current_n, 2)
+        self.assertEqual(pj._name, "tjuxt")
+        self.assertEqual(pj._latex_code, "")
+        self.assertEqual(pj._n_args, -1)
+        self.assertEqual(pj.pp, PublicProperties())
+
+        pj.current_n = 4
+        self.assertEqual(pj.current_n, 4)
+        pj2 = TJuxt(4)
+        self.assertEqual(pj, pj2)
+
+        pj = TJuxt(pp=PublicProperties(color=3, style=5))
+        self.assertEqual(pj.current_n, 2)
+        self.assertEqual(pj._name, "tjuxt")
+        self.assertEqual(pj._latex_code, "")
+        self.assertEqual(pj._n_args, -1)
+        self.assertEqual(pj.pp, PublicProperties(color=3, style=5))
+
+        with self.assertRaises(TypeError):
+            TJuxt(PublicProperties(color=3, style=5))
+
+        for n in range(-5, 2):
+            with self.assertRaises(ValueError):
+                TJuxt(n)
+
+if __name__ == '__main__':
+    unittest.main()
--- visualequation/ops.py
+++ visualequation/ops.py
@@ -24,17 +24,57 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from typing import List, Union, Iterable, Optional
+from typing import Optional
+from copy import deepcopy
 
-class Op:
-    """Class for primitive elements of an equation."""
+# Class to manage properties. None is a default value for each field,
+# associated to particular value that must be also explicitly chosen
+PP_KEYS = ("color", "font", "style")
 
-    def __init__(self, name: str, latex_code: str, n_args = 0,
-                 type_: Optional[str] = None):
-        self.name = name
-        self.latex_code = latex_code
-        self.n_args = n_args
-        self.type_ = type_ if type_ is not None else ""
+
+class PublicProperties(dict):
+    def __init__(self, **kwargs):
+        super().__init__(dict.fromkeys(PP_KEYS))
+        for k, v in kwargs.items():
+            if k not in self:
+                raise KeyError(repr(k) + " is not a public property.")
+            self[k] = v
+
+    def __setitem__(self, k, v):
+        if k not in PP_KEYS:
+            raise KeyError(repr(k) + " is not a public property.")
+        super().__setitem__(k, v)
+
+
+PSEUDOSYMB_WRONG_PP_ARG_ERROR_MSG = "pp must be a PublicProperties"
+PSEUDOSYMB_PP_MIXED_ERROR_MSG = "pp must be a PublicProperties"
+
+
+class PseudoSymb:
+    def __init__(self, name: str, latex_code: str,
+                 pp: Optional[PublicProperties] = None, **kwargs):
+        """Create a PseudoSymb.
+
+        .. note::
+            If pp parameter is used it is kept as a reference. You may want to
+            pass a copy of an already used PublicProperties.
+        """
+        if not isinstance(name, str):
+            raise TypeError("Parameter name must be a str.")
+        if not isinstance(latex_code, str):
+            raise TypeError("Parameter latex_code must be a str.")
+        self._name = name
+        self._latex_code = latex_code
+
+        if pp is not None:
+            if not isinstance(pp, PublicProperties):
+                raise TypeError(PSEUDOSYMB_WRONG_PP_ARG_ERROR_MSG)
+            if kwargs:
+                raise TypeError(PSEUDOSYMB_PP_MIXED_ERROR_MSG)
+            self.pp = pp
+        else:
+            self.pp = PublicProperties(**kwargs)
+
 
     def __eq__(self, other):
         if type(self) is type(other):
@@ -45,31 +85,200 @@ class Op:
     def __ne__(self, other):
         return not self == other
 
+    def _repr_private(self):
+        return repr(self._name) + ", " + repr(self._latex_code)
+
+    def _repr_pp(self):
+        s_repr = ""
+        for k, v in self.pp.items():
+            if v is not None:
+                s_repr += ", " + k + "=" + repr(v)
+
+        return s_repr
+
     def __repr__(self):
-        if self.type_ == '' and not self.n_args:
-            return "Op(" + repr(self.name) + ", " + repr(self.latex_code) + ")"
-        if self.type_ == '':
-            return "Op(" + repr(self.name) + ", " + repr(self.latex_code) \
-                   + ", " + repr(self.n_args) + ")"
-        return "Op(" + repr(self.name) + ", " + repr(self.latex_code) \
-               + ", " + repr(self.n_args) + ", " + repr(self.type_) + ")"
+        return "PseudoSymb(" + self._repr_private() + self._repr_pp() + ")"
 
     def __str__(self):
-        return self.name.upper()
+        return self._name.upper()
 
     def __hash__(self):
-        # str(self) would be enough
-        return hash(repr(self))
+        # Names must be unique for different instances
+        return hash(self._name)
 
 
+class Op(PseudoSymb):
+    """Class for primitive elements of an equation."""
+
+    def __init__(self, name: str, latex_code: str,
+                 n_args: int = 1, pref_arg: int = 1, lo_base: bool = False,
+                 user_sel: bool = True,
+                 pp: Optional[PublicProperties] = None,
+                 **kwargs):
+        super().__init__(name, latex_code, pp=pp, **kwargs)
+        if not isinstance(n_args, int):
+            raise TypeError("Parameter n_args must be an int.")
+        if n_args == 0 or n_args < -1:
+            raise ValueError("Parameter n_args must be positive or -1.")
+
+        if not isinstance(pref_arg, int):
+            raise TypeError("Parameter pref_arg must be an int.")
+        if pref_arg < 1:
+            raise ValueError("Parameter pref_arg must be positive.")
+
+        if not isinstance(lo_base, bool):
+            raise TypeError("Parameter lo_base must be a bool.")
+
+        if not isinstance(user_sel, bool):
+            raise TypeError("Parameter user_sel must be a bool.")
+
+        self._n_args = n_args
+        self._pref_arg = pref_arg
+        self._lo_base = lo_base
+        self._user_sel = user_sel
+
+    def _repr_private_aux(self, no_n_args=False, no_pref_arg=False,
+                          no_lo_base=False, no_user_sel=False):
+        s_repr = ""
+        if not no_n_args and self._n_args != 1:
+            s_repr += ", n_args=" + repr(self._n_args)
+        if not no_pref_arg and self._pref_arg != 1:
+            s_repr += ", pref_arg=" + repr(self._pref_arg)
+        if not no_lo_base and self._lo_base:
+            s_repr += ", lo_base=True"
+        if not no_user_sel and not self._user_sel:
+            s_repr += ", user_sel=False"
+
+        return s_repr
+
+    def __repr__(self):
+        return "Op(" + self._repr_private() + self._repr_pp() \
+               + self._repr_private_aux() + ")"
+
+    def rstep(self, arg_ord: Optional[int] = None):
+        """Return ordinal of argument to select after pressing RIGHT.
+
+        :param arg_ord: Ordinal of parameter already selected. None means that
+                       no parameter is already selected.
+        :return: Ordinal of parameter to select. None means "continue outside".
+        """
+        if arg_ord is None:
+            return 1
+        elif not isinstance(arg_ord, int) \
+                or 1 > arg_ord or arg_ord > self._n_args:
+            raise ValueError("Argument " + str(arg_ord) + " is not valid.")
+        return arg_ord + 1 if arg_ord != self._n_args else None
+
+    def lstep(self, arg_ord: Optional[int] = None):
+        """Return ordinal of argument to select after pressing LEFT.
+
+        :param arg_ord: Ordinal of parameter already selected. None means that
+                       no parameter is already selected.
+        :return: Ordinal of parameter to select. None means "continue outside".
+        """
+        if arg_ord is None:
+            return self._n_args
+        elif not isinstance(arg_ord, int) \
+                or 1 > arg_ord or arg_ord > self._n_args:
+            raise ValueError("Argument " + str(arg_ord) + " is not valid.")
+        return arg_ord - 1 if arg_ord != 1 else None
+
+    def ustep(self, arg_ord: Optional[int] = None):
+        """Return ordinal of argument to select after pressing UP.
+
+        :param arg_ord: Ordinal of parameter already selected. None means that
+                       no parameter is already selected.
+        :return: Ordinal of parameter to select. None means "continue outside".
+        """
+        if arg_ord is None:
+            return None
+        elif not isinstance(arg_ord, int) \
+                or 1 > arg_ord or arg_ord > self._n_args:
+            raise ValueError("Argument " + str(arg_ord) + " is not valid.")
+        return None
+
+    def dstep(self, arg_ord: Optional[int] = None):
+        """Return ordinal of argument to select after pressing DOWN.
+
+        :param arg_ord: Ordinal of parameter already selected. None means that
+                       no parameter is already selected.
+        :return: Ordinal of parameter to select. None means "continue outside".
+        """
+        if arg_ord is None:
+            return None
+        elif not isinstance(arg_ord, int) or 1 > arg_ord \
+                or arg_ord > self._n_args:
+            raise ValueError("Argument " + str(arg_ord) + " is not valid.")
+        return None
+
+
+class PJuxt(Op):
+    def __init__(self, initial_n: int = 2, **kwargs):
+        super().__init__("pjuxt", "", n_args=-1, user_sel=False, **kwargs)
+        if not isinstance(initial_n, int):
+            raise TypeError("Parameter initial_n must be an int.")
+        if initial_n < 2:
+            raise ValueError("Parameter initial_n must be bigger than 1.")
+        self.current_n = initial_n
+
+    def __repr__(self):
+        return "PJuxt(" + repr(self.current_n) + self._repr_pp() + ")"
+
+    def equiv_tjuxt(self):
+        return TJuxt(self.current_n, pp = deepcopy(self.pp))
+
+    def rstep(self, arg_ord: Optional[int] = None):
+        """Return ordinal of argument to select after pressing RIGHT.
+
+        :param arg_ord: Ordinal of parameter already selected. None means that
+                       no parameter is already selected.
+        :return: Ordinal of parameter to select. None means "continue outside".
+        """
+        if arg_ord is None:
+            return 1
+        elif not isinstance(arg_ord, int) \
+                or 1 > arg_ord or arg_ord > self.current_n:
+            raise ValueError("Argument " + str(arg_ord) + " is not valid.")
+        return arg_ord + 1 if arg_ord != self.current_n else None
+
+    def lstep(self, arg_ord: Optional[int] = None):
+        """Return ordinal of argument to select after pressing LEFT.
+
+        :param arg_ord: Ordinal of parameter already selected. None means that
+                       no parameter is already selected.
+        :return: Ordinal of parameter to select. None means "continue outside".
+        """
+        if arg_ord is None:
+            return self.current_n
+        elif not isinstance(arg_ord, int) \
+                or 1 > arg_ord or arg_ord > self.current_n:
+            raise ValueError("Argument " + str(arg_ord) + " is not valid.")
+        return arg_ord - 1 if arg_ord != 1 else None
+
+
+class TJuxt(Op):
+    def __init__(self, initial_n: int = 2, **kwargs):
+        super().__init__("tjuxt", "", n_args=-1, **kwargs)
+        self.current_n = initial_n
+
+        if not isinstance(initial_n, int):
+            raise TypeError("Parameter initial_n must be an int.")
+        if initial_n < 2:
+            raise ValueError("Parameter initial_n must be bigger than 1.")
+        self.current_n = initial_n
+
+    def equiv_pjuxt(self):
+        return PJuxt(self.current_n, pp = deepcopy(self.pp))
+
+
+# The following PseudoSymbs instances are defined to here because they are not
+# supposed to be modified
 SELARG = Op("selarg", r'\cdots')
-PVOID = Op("pvoid", r'\begingroup\color{purple}\oblong\endgroup')
-TVOID = Op("tvoid", r'\begingroup\color{lightgray}\oblong\endgroup')
+#PVOID = Op("pvoid", r'\begingroup\color{purple}\oblong\endgroup')
+PVOID = Op("pvoid", r'\oblong')
+#TVOID = Op("tvoid", r'\begingroup\color{lightgray}\oblong\endgroup')
+#TVOID = Op("tvoid", r'\oblong')
+#IEDIT = Op("ledit", r'\left\lgroup {0} \right\rmoustache', 1)
+IEDIT = Op("ledit", r'\begingroup\color{{blue}}{0}\endgroup', n_args=1)
+OEDIT = Op("sedit", r'\left\rmoustache {0} \right\lmoustache', n_args=1)
 
-REDIT = Op("redit", r'\left\lmoustache {0} \right\rgroup', 1)  # right
-LEDIT = Op("ledit", r'\left\lgroup {0} \right\rmoustache', 1)  # left
-NEDIT = Op("nedit", r'\left\lmoustache {0} \right\rmoustache', 1)  # new
-SEDIT = Op("sedit", r'\left\rmoustache {0} \right\lmoustache', 1)  # substitute
-PJUXT = Op("pjuxt", r'', -1)
-TJUXT = Op("tjuxt", r'', -1)
-GOP = Op("gop", r'{0}', 1)
